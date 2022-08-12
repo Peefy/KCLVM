5829271d10f0db759f9a280880647bbe42a69e26
peefy <xpf6677@163.com>
2022-08-12 10:31:43 +0800

    chore: bump kclvm checksum to d6a5247bbfd8c157ac132105f4f57475

--- kclvm/version/src/lib.rs
+++ kclvm/version/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright 2021 The KCL Authors. All rights reserved.
 
 pub const VERSION: &str = "0.4.2";
-pub const CHECK_SUM: &str = "e07ed7af0d9bd1e86a3131714e4bd20c";
+pub const CHECK_SUM: &str = "d6a5247bbfd8c157ac132105f4f57475";
 
 pub fn get_full_version() -> String {
     format!("{}-{}", VERSION, CHECK_SUM)
