0c8fd143a344d8e3d7c72e4f7e2319497f46b933
Peefy <xpf6677@163.com>
2023-07-28 19:38:14 +0800

    fix: wrong build scripts.

--- scripts/build-windows/build_kclvm_plugin_py.bat
+++ scripts/build-windows/build_kclvm_plugin_py.bat
@@ -4,9 +4,8 @@ setlocal
 cd %~dp0
 
 :: install kclvm-plugin python module
-cd ..\..\kclvm\plugin
-go run .\copy-file.go -src=.\kclvm_plugin.py     -dst=..\..\scripts\build-windows\_output\kclvm-windows\lib\site-packages\kclvm_plugin.py
-go run .\copy-file.go -src=.\kclvm_runtime.py    -dst=..\..\scripts\build-windows\_output\kclvm-windows\lib\site-packages\kclvm_runtime.py
+go run .\copy-file.go -src=..\..\kclvm\plugin\kclvm_plugin.py     -dst=..\..\scripts\build-windows\_output\kclvm-windows\lib\site-packages\kclvm_plugin.py
+go run .\copy-file.go -src=..\..\kclvm\plugin\kclvm_runtime.py    -dst=..\..\scripts\build-windows\_output\kclvm-windows\lib\site-packages\kclvm_runtime.py
 
 cd %~dp0
 go run .\copy-dir.go ..\..\plugins ..\..\scripts\build-windows\_output\kclvm-windows\plugins
